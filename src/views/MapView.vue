73aa618ee24321e7274e1b6c4e9f26c4dc4a0250
Ahmad Marjuki <138131234+kizuky75@users.noreply.github.com>
2023-11-05 00:44:27 +0700

    Update_MapView.vue

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -101,7 +101,7 @@ export default {
     async loadSomeGeoJson() {
       const nextIndex = this.layerData.length;
 
-      const response = await fetch("/Desa_Malasari.json");
+      const response = await fetch("/Malasari.json");
       const data = await response.json();
 
       console.log(data);
